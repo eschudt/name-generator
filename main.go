d2011f0fd7f3aa64f67ee6822a49e21f40f0b758
Erich Schudt <erich_schudt@live.com>
2018-07-11 19:16:43 +0100

    Added multiple handlers for mux and test file

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
-	"strings"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message + "\n"
-	w.Write([]byte(message))
+type Hello struct {
+	Message string
+}
+
+type Name struct {
+	FirstName string
+	LastName  string
 }
 
 func main() {
-	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", helloHandler)
+	mux.HandleFunc("/name", nameHandler)
+	http.ListenAndServe(":8080", mux)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	data := Hello{"Hello World"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	resp, err := json.Marshal(data)
+	if err != nil {
 		panic(err)
 	}
+	w.Write(resp)
+}
+
+func nameHandler(w http.ResponseWriter, r *http.Request) {
+	data := Name{"John", "Smith"}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	resp, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+	w.Write(resp)
 }
